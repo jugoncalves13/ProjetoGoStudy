3f4956b36a53b900ad28e78244bbcc5f64ec0c93
Anagreatti07 <ana.greatti10@gmail.com>
2024-04-02 13:45:49 -0300

    continuação

---
+++ src/Homeprincipal.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    
+    <View style={styles.container}>
+      <Text style={styles.heading}>Bem-vindo ao GoStudy!</Text>
+
+      <Text style={styles.description}>Seja bem-vindo ao nosso site de estudos GoStudy.</Text>
+      <Image style={styles.img} source={require("../assets/imagemhome.jpg")}/>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Tutoriais')}>
+            
+        <Text style={styles.buttonText}>Explorar Tutoriais</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Recursos')}>
+        <Text style={styles.buttonText}>Ver Recursos</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  description: {
+    fontSize: 16,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: '#007bff',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  img:{
+    height: 200,
+    width: 350,
+    alignItems: "center",
+   
+  }
+});
+
+export default HomeScreen;
--- src/Rotas.js
+++ src/Rotas.js
@@ -7,6 +7,9 @@ import ItensAprovados from './ItensAprovados';
 import MediaNota from './MediaNota';
 
 import Login from './Login';
+import Agenda from './Agenda';
+import Home from './Home';
+import HomeScreen from './Homeprincipal';
 
 const Tab = createBottomTabNavigator();
 
@@ -22,6 +25,9 @@ export default function Rotas() {
             <Tab.Navigator>
                 <Tab.Screen name="Aprovados" component={ItensAprovados} />
                 <Tab.Screen name="Calculadora" component={MediaNota} />
+                <Tab.Screen name="Agenda" component={Agenda} />
+                <Tab.Screen name="Home" component={Home} />
+                <Tab.Screen name="HomeScreen" component={HomeScreen} />
             </Tab.Navigator>
         </NavigationContainer>
     )
